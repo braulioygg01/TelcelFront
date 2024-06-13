2533d957179fc475fe9e3d029beb2cd643f34a53
braulioygg01 <151060603+braulioygg01@users.noreply.github.com>
2024-06-13 10:48:01 -0600

    Delete backend directory

--- backend/routes/repairs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createRepair, getRepairs } = require('../controllers/repairController');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.post('/repairs', authMiddleware, createRepair);
-router.get('/repairs', authMiddleware, getRepairs);
-
-module.exports = router;
