b2d683bdb3416a26eb69decbfcaef30c4a069798
braulioygg01 <151060603+braulioygg01@users.noreply.github.com>
2024-06-19 21:10:01 -0600

    Delete backend directory

--- backend/models/SparePart.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const SparePartSchema = new mongoose.Schema({
-  nombreRefaccion: String,
-  precio: Number,
-  cantidad: Number,
-  descripcion: String,
-  estatus: String
-});
-module.exports = mongoose.model('SparePart', SparePartSchema);
